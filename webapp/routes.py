5d20df7f8ca9fb883a69fe2a03bb9f94d0feb542
pimgeek <wudi.biz@gmail.com>
2018-06-27 01:15:36 +0800

    在网页代码中引入变量

--- webapp/routes.py
+++ webapp/routes.py
@@ -3,4 +3,14 @@ from webapp import flask_obj
 @flask_obj.route('/')
 @flask_obj.route('/index')
 def index():
-    return '你好，世界！\n'
+    users = { 'username': 'pimgeek' }
+    return '''
+<html>
+    <head>
+        <title>首页 - 引思网卡片库</title>
+    </head>
+    <body>
+        <h1>你好，''' + users['username'] + '''！</h1>
+    </body>
+</html>
+    '''
