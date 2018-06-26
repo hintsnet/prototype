2bc67be8a9667c67ab9c8fba60842118ee274726
pimgeek <wudi.biz@gmail.com>
2018-06-27 01:22:30 +0800

    把 index 页面的代码提取为单独的模板文件

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,16 +1,8 @@
+from flask import render_template
 from webapp import flask_obj
 
 @flask_obj.route('/')
 @flask_obj.route('/index')
 def index():
     users = { 'username': 'pimgeek' }
-    return '''
-<html>
-    <head>
-        <title>首页 - 引思网卡片库</title>
-    </head>
-    <body>
-        <h1>你好，''' + users['username'] + '''！</h1>
-    </body>
-</html>
-    '''
+    return render_template('index.html', title='首页', users=users)
