dccb9f049bd6d674ea47fc16457ffc6f5403739d
pimgeek <wudi.biz@gmail.com>
2018-06-27 22:42:40 +0800

    在模板代码中尝试使用 for 循环，获取列表数据

--- webapp/routes.py
+++ webapp/routes.py
@@ -4,5 +4,15 @@ from webapp import flask_obj
 @flask_obj.route('/')
 @flask_obj.route('/index')
 def index():
-    users = { 'username': 'pimgeek' }
-    return render_template('index.html', users=users)
+    myusers = { 'username': 'pimgeek' }
+    myposts = [
+        {
+            'author': { 'username': 'pimgeek' },
+            'body': '一切有为法，皆梦幻泡影。'
+        },
+        {
+            'author': { 'username': 'ceciliapple' },
+            'body': 'Smelly Cat, Smelly Cat, What Do They Feed You?'
+        }
+    ]
+    return render_template('index.html', users=myusers, posts=myposts)
