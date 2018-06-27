8392e90e6abd95f90191bf60e49753aa5c74cb06
pimgeek <wudi.biz@gmail.com>
2018-06-27 22:28:56 +0800

    试用模板中的 if .. else 语句

--- webapp/routes.py
+++ webapp/routes.py
@@ -5,4 +5,4 @@ from webapp import flask_obj
 @flask_obj.route('/index')
 def index():
     users = { 'username': 'pimgeek' }
-    return render_template('index.html', title='首页', users=users)
+    return render_template('index.html', users=users)
