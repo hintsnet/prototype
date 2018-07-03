32d098d759a4be0beb474a469f4b5337751a9f16
pimgeek <wudi.biz@gmail.com>
2018-07-03 16:19:52 +0800

    利用 config.py 引入配置类 Config，专门存储应用配置信息

--- hintsnet.py
+++ hintsnet.py
@@ -1,1 +1,2 @@
 from webapp import flask_obj
+print(" * Preset secret key is: %s" % flask_obj.config['SECRET_KEY'])
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 flask_obj = Flask(__name__)
+flask_obj.config.from_object(Config)
 
 from webapp import routes
