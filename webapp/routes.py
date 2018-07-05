8f1194c5018a2c07e2aed1c0cea402a014c43d64
pimgeek <wudi.biz@gmail.com>
2018-07-05 16:05:43 +0800

    1. 添加服务端处理逻辑，允许提交用户名/密码和记住登录信息的选项 2. 当登录信息输入不完整时，提交后将留在 login 页面并显示出错信息 3. 当登录信息输入完整时，提交后将跳转到 index 页面并显示登录名等信息 4. 注意：目前尚未实现真正的登录逻辑，只是“假登录”

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from webapp import flask_obj
 from webapp.forms import LoginForm
 
@@ -19,8 +19,17 @@ def index():
     page_view = render_template('index.html', users=my_users, posts=my_posts)
     return page_view
 
-@flask_obj.route('/login')
+@flask_obj.route('/login', methods=['GET','POST'])
 def login():
     my_form = LoginForm()
-    page_view = render_template('login.html', title='登录', form=my_form)
+    if my_form.validate_on_submit():
+        flash('用户 %s 尝试登录，是否要求记忆登录信息？%s' % \
+            (my_form.username.data, my_form.remember.data))
+        page_view = redirect('/index')
+    else:
+        if (my_form.username.data or my_form.password.data or my_form.remember.data):
+            flash('用户输入内容有误，请重新输入...')
+        else:
+            pass
+        page_view = render_template('login.html', title='登录', form=my_form)
     return page_view
