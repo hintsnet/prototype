4cc257ac5f1f36b056f04bcb6f91718cac8e7607
pimgeek <wudi.biz@gmail.com>
2019-02-02 14:11:23 +0800

    1 在 .gitignore 中增加 preload.py 和 .flaskenv 两个需要忽略的文件   引入 preload.py，用于在开发过程中，启动 python 解释器之前 调用一些预置命令   引入 .flaskenv，用于在运行开发版程序时，自动载入一些环境变量 2 修改 run.bat 启动脚本，去除关于环境变量设置的提示信息

--- run.bat
+++ run.bat
@@ -1,17 +1,8 @@
 @echo off
 
-REM 把 cmd.exe 的代码页设置为 utf8（65001）
 chcp 65001
-echo "设置 flask app 的入口脚本"
-set FLASK_APP=hintsnet.py
+echo 把 cmd.exe 的代码页设置为 utf8（65001）
 
-echo "设置 flask app 的运行环境参数"
-echo "请手动设置 APP 密钥，千万不要写入源码文件！"
-echo set APP_SECRET_KEY=...
-echo "请手动设置 NEO4J 数据库配置信息，千万不要写入源码文件！"
-echo set NEO4J_DB_URI=...
-echo set NEO4J_DB_USR=...
-echo set NEO4J_DB_KEY=...
+echo 启动 flask app [hintsnet]
 
-echo "启动 flask app"
 flask run
