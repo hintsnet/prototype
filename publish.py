7da85923169266bbc93446f8bedb9c3b9dceb128
pimgeek <wudi.biz@gmail.com>
2019-04-03 11:18:17 +0800

    1 代码缩进统一为用制表符控制 2 添加可生成节点索引的 gen_thought_index 函数 3 添加可获取当前节点所有相关媒体文件列表的 get_media_list 函数 4 添加可导出当前节点笔记为可对外发布的 html 内容的 get_thought_detail 函数   其中包含上传图片到阿里云 oss 的代码 5 添加用于对整个文件内容进行查找替换的 file_replace_with 函数 6 添加用于生成公开 html 文件的 gen_html_page 函数 7 修复 cmd.exe 中的中文打印问题

--- publish.py
+++ publish.py
@@ -3,6 +3,7 @@ from config import Config
 from shutil import copyfile
 import filecmp
 import os
+import oss2
 import sqlite3
 import sys
 
@@ -22,32 +23,107 @@ def connect_sqlite_db(db_file_path):
 
 # 通用的数据查询函数, 用于执行任意 sql 语句
 def query_db(db_cursor, sql):
-    db_cursor.execute(sql)
-    records = db_cursor.fetchall()
-    return records
+	db_cursor.execute(sql)
+	records = db_cursor.fetchall()
+	return records
 
 # 获取所有标记为"可发布"的节点的 id, 以列表形式返回
 def get_pub_thought_ids(db_cursor):
-    # 定义 sql 语句
-    sql = """
-    select dest.id
-    from thoughts as src inner join links inner join thoughts as dest
-    where src.id = thoughtIdA and dest.id = thoughtIdB and
-    links.meaning = 5 and src.name = "可发布"
-    """
-    results = query_db(db_cursor, sql)
-    thought_ids = [result[0] for result in results]
-    return thought_ids
+	# 定义 sql 语句
+	sql = """
+	select dest.id
+	from thoughts as src inner join links inner join thoughts as dest
+	where src.id = thoughtIdA and dest.id = thoughtIdB and
+	links.meaning = 5 and src.name = "可发布"
+	"""
+	results = query_db(db_cursor, sql)
+	thought_ids = [result[0] for result in results]
+	return thought_ids
 
 # 给定一组节点 id, 获取相关节点的详细信息
 # 目前, 只获取 name 字段的内容
 def get_thought_data(db_cursor, thought_ids):
-    thought_data = []
-    for thought_id in thought_ids:
-        sql = 'select name from thoughts where id="%s"' % thought_id
-        results = query_db(db_cursor, sql)
-        thought_data.append({'id': thought_id,'name': results[0][0]})
-    return thought_data
+	thought_data = []
+	for thought_id in thought_ids:
+		sql = 'select name from thoughts where id="%s"' % thought_id
+		results = query_db(db_cursor, sql)
+		thought_data.append({'id': thought_id,'name': results[0][0]})
+	return thought_data
+
+# 生成待发布节点列表的 html list
+def gen_thought_index(thought_data):
+	thought_items = ""
+	for thought in thought_data:
+		thought_items += '<li><a href="%s.html">%s</a></li>\n' % (thought['id'], thought['name'])
+	index_body = "<ul>\n%s</ul>" % thought_items
+	return index_body
+
+# 获取节点的附件(多媒体文件)列表
+def get_media_list(thought_id, local_dir):
+	media_list = os.listdir("%s/%s/Notes" % (local_dir, thought_id))
+	media_list.remove('notes.html')
+	return media_list
+
+# 获取节点的笔记内容(如果有的话)
+def get_thought_detail(thought_data, local_dir, pub_dir):
+	for thought in thought_data:
+		src_note = '%s/%s/Notes/notes.html' % (local_dir, thought['id'])
+		dest_note = '%s/%s.html' % (pub_dir, thought['id'])
+		if os.path.isfile(src_note):
+			copyfile(src_note, dest_note)
+			file_replace_with(dest_note, '<!--BrainNotesBase-->', 'https://pimfans.oss-cn-beijing.aliyuncs.com/hintsnet/tb/media')
+			with open(dest_note, "r", encoding="utf-8") as note_fh:
+				note_content = note_fh.read()
+			with open(dest_note, "w", encoding="utf-8") as note_fh:
+				note_full_html = gen_html_page(thought['name'], note_content + sns_comment + '\n\n<META http-equiv=Content-Type content="text/html; charset=utf-8">')
+				note_fh.write(note_full_html)
+			media_list = get_media_list(thought['id'], local_dir)
+			for media in media_list:
+				file_replace_with(dest_note, media, media + "/eq_width")
+				src_media = '%s/%s/Notes/%s' % (local_dir, thought['id'], media)
+				dest_media = '%s/media/%s' % (pub_dir, media)
+				copyfile(src_media, dest_media)
+				res = bucket.put_object_from_file('hintsnet/tb/media/%s' % media, src_media)
+				print(res)
+		else:
+			with open(dest_note, "w", encoding="utf-8") as note_fh:
+				note_full_html = '<p>暂无笔记</p>\n\n<META http-equiv=Content-Type content="text/html; charset=utf-8">'
+				note_fh.write(note_full_html)
+	return True
+
+# 批量替换文件中满足条件的字符串
+def file_replace_with(file, match_str, subst_str):
+	new_content = ""
+	with open(file, "rt", encoding="utf-8") as fh:
+		new_content = fh.read().replace(match_str, subst_str)
+	with open(file, "w", encoding="utf-8") as fh:
+		fh.write(new_content)
+	return True
+
+# 把 html 内容写为文件
+def write_content_to_file(content, pub_dir, filename):
+	with open("%s/%s" % (pub_dir, filename), "w+", encoding="utf-8") as fh:
+		fh.write(content)
+	return True
+
+# 给定 title 和 body, 包装为 html 页面
+def gen_html_page(title, body):
+	full_html = """
+<html>
+  <head>
+	<title>%s</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+	<meta charset="utf-8">
+	<style>
+		img { width:95%%; }
+	</style>
+  </head>
+  <body>
+%s
+  </body>
+</html>
+""" % (title, body)
+	return full_html
 
 # 主程序入口处
 if(__name__ == '__main__'):
@@ -60,6 +136,8 @@ if(__name__ == '__main__'):
 	# 为安全起见, prep_env 文件不会被加入版本库
 	# 本地 TB 数据所在目录
 	local_tb_dir = Config.local_tb_dir
+	# 待发布文件所在目录
+	tb_pub_basedir = Config.tb_pub_basedir
 	# TB 数据库文件名
 	tb_db_name = Config.tb_db_name
 	# 本地 TB 数据库的完整路径
@@ -79,5 +157,23 @@ if(__name__ == '__main__'):
 	# 获取所有待发布的节点 id 列表
 	pub_thought_ids = get_pub_thought_ids(db_cursor)
 	# 获取每个节点的详细信息
-	res = get_thought_data(db_cursor, pub_thought_ids)
-	print(res)
+	thought_data = get_thought_data(db_cursor, pub_thought_ids)
+	thought_idx = gen_thought_index(thought_data)
+	
+	sns_comment = '''
+<h2>使用 GitHub 账号发表评论</h2>
+<script src="https://utteranc.es/client.js"
+	repo="hintsnet/discussions"
+	issue-term="title"
+	label="utterances"
+	theme="github-light"
+	crossorigin="anonymous"
+	async>
+</script>
+	'''
+	auth = oss2.Auth(Config.oss_acckey, Config.oss_accsec)
+	bucket = oss2.Bucket(auth, Config.oss_epoint, Config.oss_bucket)
+	
+	html_body = gen_html_page(u"引思卡片索引", thought_idx)
+	gen_html_page(html_body,"index.html")
+	get_thought_detail(thought_data, local_tb_dir, tb_pub_basedir)
